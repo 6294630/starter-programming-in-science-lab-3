e8113ab716653cd77bfbb0250f028c4d9935c7d1
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-02-07 01:47:19 -0500

    Create Lab3.py
    Lab3.py updated.

---
+++ Lab3.py
@@ -0,0 +1,17 @@
+# Function 1 (30): Convert the given polar coordinates (r,θ) to Cartesian coordinates (x,y). 
+# This function should take the polar coordinates (r,θ) and return Cartesian coordinates (x,y).
+def polar_to_cartesian(r,θ):
+    # TODO: Implement this function
+    pass  # Replace with your code
+
+# Function 2(30): Convert Cartesian coordinates (x,y) to polar coordinates (r,θ).
+# This function should take the Cartesian coordinates (x,y) as input and return the polar coordinates (r,θ).
+def cartesian_to_polar(x,y):
+    # TODO: Implement this function
+    pass  # Replace with your code
+
+# Function 3 (40): Calculate the position of pendulum for (A, f, ϕ, t).
+# This function should take (A, f, ϕ, t) as input and return the position value x.
+def cartesian_to_polar(x,y):
+    # TODO: Implement this function
+    pass  # Replace with your code
