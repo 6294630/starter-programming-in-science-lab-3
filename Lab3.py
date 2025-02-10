b6cb656cb70eb2ae8ce584b61ccb399b642f8146
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-02-10 12:55:20 -0500

    Update Lab3.py
    phi and theta remarked

--- Lab3.py
+++ Lab3.py
@@ -1,17 +1,17 @@
 # Function 1 (30): Convert the given polar coordinates (r,θ) to Cartesian coordinates (x,y). 
-# This function should take the polar coordinates (r,θ) and return Cartesian coordinates (x,y).
+# This function should take the polar coordinates (r,θ) and return Cartesian coordinates (x,y), where θ=theta.
 def polar_to_cartesian(r,θ):
     # TODO: Implement this function
     pass  # Replace with your code
 
-# Function 2(30): Convert Cartesian coordinates (x,y) to polar coordinates (r,θ).
+# Function 2(30): Convert Cartesian coordinates (x,y) to polar coordinates (r,θ) , where θ=theta.
 # This function should take the Cartesian coordinates (x,y) as input and return the polar coordinates (r,θ).
 def cartesian_to_polar(x,y):
     # TODO: Implement this function
     pass  # Replace with your code
 
-# Function 3 (40): Calculate the position of pendulum for (A, f, ϕ, t).
-# This function should take (A, f, ϕ, t) as input and return the position value x=A*sin(2*π*f*t+Φ) .
+# Function 3 (40): Calculate the position of pendulum for (A, f, Φ, t), where Φ=phi.
+# This function should take (A, f, Φ, t) as input and return the position value x=A*sin(2*π*f*t+Φ) .
 def calculate_position(A, f, Φ, t):
     # TODO: Implement this function
     pass  # Replace with your code
